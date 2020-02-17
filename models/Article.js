c113a8fd362a9c9c110c865449fe792513393673
Brittany Huber <brittanyleighhuber@gmail.com>
2020-02-16 22:26:57 -0700

    Added unique to article model

--- models/Article.js
+++ models/Article.js
@@ -12,7 +12,8 @@ var ArticleSchema = new Schema({
     },
     link: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     saved: {
         type: Boolean,
